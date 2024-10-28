4aa56be3cedb4b36d8b2f5b4e753d1dad82bf593
anyuegonzalez <anyuee07@gmail.com>
2024-10-28 17:32:04 +0100

    jftyj

--- Assets/Scripts/FirstPerson.cs
+++ Assets/Scripts/FirstPerson.cs
@@ -1,20 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FirstPerson : MonoBehaviour
 {
-    [SerializeField] private float velocidadMovimiento;
+    [SerializeField] private float velocidadMovimiento, escalaGravedad,radioDeteccion;
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private Vector3 movimientoVertical;
+    [SerializeField] private LayerMask queEsSuelo;
+    [SerializeField] private Transform pies;
+    [SerializeField] private float alturaSalto;
 
-    CharacterController controller;
-    private Camera cam;
     void Start()
     {
         // bloquea el raton en el centro de la pantalla y lo oculta
-        Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.lockState = CursorLockMode.Locked;
        controller = GetComponent<CharacterController>();
-
-        cam = Camera.main;
     }
 
     
@@ -22,30 +24,46 @@ public class FirstPerson : MonoBehaviour
     {
         float h = Input.GetAxisRaw("Horizontal"); 
         float v = Input.GetAxisRaw("Vertical");
-
-
-        //Vector3 movimiento = new Vector3(h, 0, v ).normalized;
-        
         Vector2 input = new Vector2(h, v).normalized;
-
-        // si existe input...
-        // sqrMagnitude es mas optimo que magnitude
+        float angulo = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
         if(input.sqrMagnitude > 0)
         {
-            // mse calcula el angulo al q tengo q rotarle en funcion de los input y en fuincion hacia donde este orientada la camara
-            float anguloRotcion = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
+            Vector3 movimiento = Quaternion.Euler(0, angulo, 0) * Vector3.forward;
+            controller.Move(movimiento * 5 * Time.deltaTime);
+        }
+        AplicarGravedad();
+        DeteccionSuelo();
+     
+    }
+    private void AplicarGravedad()
+    {
+        // mi mov v en la y va aumentando a cierta escala por segundo
+        movimientoVertical.y += escalaGravedad * Time.deltaTime;
+        controller.Move(movimientoVertical * Time.deltaTime);
+    }
+    private void DeteccionSuelo()
+    {
+        // tengo que lanzar una bola de deteccion en mis pies para detectar si 
+        Collider[] collsDetectados = Physics.OverlapSphere(pies.position, radioDeteccion, queEsSuelo);
+        // si existe al menos un collider bajo mis pies
+        if(collsDetectados.Length > 0)
+        {
+            movimientoVertical.y = 0;
+            Saltar();
+        }
 
-            transform.eulerAngles = new Vector3(0, anguloRotcion, 0);
-
-            Vector3 movimiento = Quaternion.Euler(0, anguloRotcion, 0) * Vector3.forward;
-            
-            controller.Move(movimiento * velocidadMovimiento * Time.deltaTime);
-        } 
-
-
-
-       
-
-
+    }
+    // esto sirve para dibujar cualquier figura en la escena
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(pies.position, radioDeteccion);
+    }
+    private void Saltar()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            movimientoVertical.y = Mathf.Sqrt(-2 * escalaGravedad * alturaSalto);
+        }
     }
 }
