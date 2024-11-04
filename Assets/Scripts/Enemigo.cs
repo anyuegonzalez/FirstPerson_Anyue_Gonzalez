bc32f3b0acca759f30a2d6785f3c1dc62703d478
anyuegonzalez <anyuee07@gmail.com>
2024-11-04 16:42:31 +0100

    gre

--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Rendering;
 
 public class Enemigo : MonoBehaviour
 {
     private NavMeshAgent agent;
     private FirstPerson player;
-    private bool attacking = false;
+    private bool ventanaAbierta = false;
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] float radioDeteccion;
+    [SerializeField] private LayerMask jugador;
 
     private Animator anim;
    
@@ -20,20 +24,33 @@ public class Enemigo : MonoBehaviour
     }  
     void Update()
     {
+        Perseguir();
+
+        if(ventanaAbierta)
+        {
+           DetectarJugador();
+        }
+    }
+    private void DetectarJugador()
+    {
+        Collider[] collJugador = Physics.OverlapSphere(attackPoint.position, radioDeteccion, jugador);
+    }
+
+    private void Perseguir()
+    {
         // tengo q definir como destino la posicion del player
         agent.SetDestination(player.transform.position);
 
 
         // si la distancia q nos queda hacia el objeto cae por debajo del stoppingDistance
-        if(agent.remainingDistance <= agent.stoppingDistance)
+        if (agent.remainingDistance <= agent.stoppingDistance)
         {
             // me paro ante el
             agent.isStopped = true;
             anim.SetBool("Attacking", true);
         }
-
     }
-    
+
     #region Eventos de animación
     private void FinAtaque()
     {
@@ -43,11 +60,11 @@ public class Enemigo : MonoBehaviour
     }
     private void AbrirVenatanaAtaque()
     {
-
+        ventanaAbierta = true;
     } 
     private void CerrarVenatanaAtaque()
     {
-
+        ventanaAbierta = false;
     }
     #endregion
 
