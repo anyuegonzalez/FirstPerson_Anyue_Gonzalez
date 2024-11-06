3aa0e1500a56e43caf1e773e287f6b36d74a054e
anyuegonzalez <anyuee07@gmail.com>
2024-11-06 21:25:52 +0100

    few

--- Assets/Scripts/ArmaManual.cs
+++ Assets/Scripts/ArmaManual.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ArmaManual : MonoBehaviour
 {
+    [SerializeField] ParticleSystem system;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +16,7 @@ public class ArmaManual : MonoBehaviour
     {
         if(Input.GetMouseButtonDown(0))
         {
-            Debug.Log("Pium!");
+            system.Play();
         }
     }
 }
