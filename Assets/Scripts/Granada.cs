bc586c73bed0223f6c7d1bfb62bfbddb79e55cae
anyuegonzalez <anyuee07@gmail.com>
2024-11-18 16:36:10 +0100

    s

--- Assets/Scripts/Granada.cs
+++ Assets/Scripts/Granada.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
+using UnityEditor.Timeline;
 using UnityEngine;
 
 public class Granada : MonoBehaviour
 {
- 
+    private Rigidbody rb;
+    [SerializeField] float fuerzaImpulso;
     void Start()
     {
-        
+        GetComponent<Rigidbody>().AddForce(transform.forward * fuerzaImpulso, ForceMode.Impulse);
     }
     void Update()
     {
