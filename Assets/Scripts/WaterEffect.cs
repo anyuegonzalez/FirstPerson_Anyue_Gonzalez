c91c9e29563cc2fd156a05ca069bbdd7572be3be
anyuegonzalez <anyuee07@gmail.com>
2024-11-21 21:20:34 +0100

    a

---
+++ Assets/Scripts/WaterEffect.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class WaterEffect : MonoBehaviour
+{
+    private Volume effect;
+    void Start()
+    {
+        effect = GetComponent<Volume>();
+    }
+    void Update()
+    {
+        //busca en tu profile si tienes el efecto lens diostorsion
+        if(effect.profile.TryGet(out LensDistortion distorsion))
+        {
+
+        }
+    }
+}
