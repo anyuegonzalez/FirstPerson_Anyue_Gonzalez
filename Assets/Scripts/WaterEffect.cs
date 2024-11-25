f0e95e5c33e8fc9053cb5c9b231c3016b92a179d
anyuegonzalez <anyuee07@gmail.com>
2024-11-25 18:02:56 +0100

    a

--- Assets/Scripts/WaterEffect.cs
+++ Assets/Scripts/WaterEffect.cs
@@ -7,16 +7,22 @@ using UnityEngine.Rendering.Universal;
 public class WaterEffect : MonoBehaviour
 {
     private Volume effect;
+    [SerializeField] private float velocidad;
     void Start()
     {
         effect = GetComponent<Volume>();
     }
     void Update()
     {
+
+       
         //busca en tu profile si tienes el efecto lens diostorsion
         if(effect.profile.TryGet(out LensDistortion distorsion))
         {
-            //distorsion.xMultiplier;
+            FloatParameter miVariable = new FloatParameter(Mathf.Cos(velocidad * Time.time));
+            FloatParameter miVariable2 = new FloatParameter(Mathf.Cos(velocidad * Time.time));
+            distorsion.xMultiplier.SetValue(miVariable);
+            distorsion.xMultiplier.SetValue(miVariable2);
         }
     }
 }
