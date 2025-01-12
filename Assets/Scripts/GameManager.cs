87161dd9d11b1598ac1ad40d60587867d48df6d4
anyuegonzalez <anyuee07@gmail.com>
2025-01-12 19:07:17 +0100

    a

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,8 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject canvasPausa;
-    public static int nivelJuego = 0;
+    [SerializeField] GameObject canvasPausa;
+    [SerializeField] static int nivelJuego = 0;
 
     private FirstPerson jugador;
     private bool estaMuerto = false;
